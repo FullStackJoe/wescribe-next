2499633b6a5a5959e3ef16c84c0a66a5d0b89020
Johan Nielsen <johanhn10@gmail.com>
2023-10-08 01:38:09 +0200

    testestest

--- src/pages/findSubscriptions.js
+++ src/pages/findSubscriptions.js
@@ -16,9 +16,6 @@ export default function AlternativeSubscriptions() {
         console.log(fetchedData); // Add this line
         setAltSubscriptionData(fetchedData);
       })
-      .then((fetchedData) => {
-        setAltSubscriptionData(fetchedData);
-      })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
@@ -26,10 +23,7 @@ export default function AlternativeSubscriptions() {
       .then((response) => response.json())
       .then((fetchedData) => {
         console.log(fetchedData); // Add this line
-        setAltSubscriptionData(fetchedData);
-      })
-      .then((fetchedData) => {
-        setSubscriptionData(fetchedData);
+        setSubscriptionData(fetchedData); // Corrected line
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
@@ -44,21 +38,22 @@ export default function AlternativeSubscriptions() {
           <div className="flex flex-col items-center">
             {AltSubscriptionData[0] ? <h2>Dine nuværende abonementer</h2> : ""}
             <div className="flex flex-wrap justify-center">
-              {SubscriptionData.map((item) =>
-                item.type === "Mobile" ? (
-                  <div className="px-6 py-5" key={item.subscriptionid}>
-                    <SubscriptionCard
-                      provider={item.provider}
-                      talk={item.talktime === 9999 ? "FRI" : item.talktime}
-                      data={item.datamonth === 9999 ? "FRI" : item.datamonth}
-                      monthlyPrice={parseInt(item.pricemonth)}
-                      editMode={""}
-                      subscriptionId={item.subscriptionid}
-                      onSubmitSuccess={""}
-                    />
-                  </div>
-                ) : null
-              )}
+              {Array.isArray(SubscriptionData) &&
+                SubscriptionData.map((item) =>
+                  item.type === "Mobile" ? (
+                    <div className="px-6 py-5" key={item.subscriptionid}>
+                      <SubscriptionCard
+                        provider={item.provider}
+                        talk={item.talktime === 9999 ? "FRI" : item.talktime}
+                        data={item.datamonth === 9999 ? "FRI" : item.datamonth}
+                        monthlyPrice={parseInt(item.pricemonth)}
+                        editMode={""}
+                        subscriptionId={item.subscriptionid}
+                        onSubmitSuccess={""}
+                      />
+                    </div>
+                  ) : null
+                )}
               {/* 
             <div className="flex flex-col mt-8 items-center">
               <p>Spar x kr pr md</p>
@@ -74,25 +69,26 @@ export default function AlternativeSubscriptions() {
           <div className="flex flex-col items-center">
             {AltSubscriptionData[0] ? <h2>Forslag til nye abonementer</h2> : ""}
             <div className="flex flex-wrap justify-center">
-              {AltSubscriptionData.map((item) =>
-                item[0].type === "Mobile" ? (
-                  <div className="px-6 py-5" key={item.subscriptionid}>
-                    <SubscriptionCard
-                      provider={item[0].provider}
-                      talk={
-                        item[0].talktime === 9999 ? "FRI" : item[0].talktime
-                      }
-                      data={
-                        item[0].datamonth === 9999 ? "FRI" : item[0].datamonth
-                      }
-                      monthlyPrice={parseInt(item[0].pricemonth)}
-                      editMode={""}
-                      subscriptionId={item[0].subscriptionid}
-                      onSubmitSuccess={""}
-                    />
-                  </div>
-                ) : null
-              )}
+              {Array.isArray(AltSubscriptionData) &&
+                AltSubscriptionData.map((item) =>
+                  item[0].type === "Mobile" ? (
+                    <div className="px-6 py-5" key={item.subscriptionid}>
+                      <SubscriptionCard
+                        provider={item[0].provider}
+                        talk={
+                          item[0].talktime === 9999 ? "FRI" : item[0].talktime
+                        }
+                        data={
+                          item[0].datamonth === 9999 ? "FRI" : item[0].datamonth
+                        }
+                        monthlyPrice={parseInt(item[0].pricemonth)}
+                        editMode={""}
+                        subscriptionId={item[0].subscriptionid}
+                        onSubmitSuccess={""}
+                      />
+                    </div>
+                  ) : null
+                )}
             </div>
           </div>
         </div>
