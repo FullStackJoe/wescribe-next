9e0df170f76da9b1f693a9d91ffd27e4008e1c98
Johan Nielsen <johanhn10@gmail.com>
2023-10-29 13:09:39 +0100

    Addded createBroadbandSubscription api endpoint

--- apiQueries.js
+++ apiQueries.js
@@ -1,16 +1,20 @@
-export const getSubscriptionsByUserIdQuery =
+// Get queries
+export const getSubscription = // Get data on a single subsription
+  "SELECT * FROM Subscriptions WHERE subscriptionId = $1;";
+export const getSubscriptionsByUserIdQuery = // Get all subscriptions assigned a specific user
   "SELECT * FROM subscriptions WHERE userId = $1;";
-export const getCheaperMobileAlternativeQuery =
+export const getCheaperMobileAlternativeQuery = // Get 3 alternative mobile subs with lower/equal price, more/equal data, more/queal talktime, which are available. sorted by price
   "SELECT * FROM Subscriptions WHERE type = $1 AND priceMonth <= $2 AND datamonth >= $3 AND talktime >= $4 AND subscriptionId <> $5 AND available = TRUE ORDER BY priceMonth ASC, datamonth DESC LIMIT 3;";
+// Create queries
+export const addUserQuery = // Add a user to Users table, returning the created user.
+  "INSERT INTO users(userID) VALUES ($1) RETURNING *;";
+export const createBroadbandSubscriptionQuery = // Create broadband sub from provider, priceMonth, uploadSpeed, downloadSpeed, userId, returns the created sub
+  "insert into Subscriptions(Provider, Type, Name, PriceMonth, DataMonth, Talktime, UploadSpeed, DownloadSpeed, Userid, available) VALUES ($1, 'Broadband', 'Placeholder', $2, NULL, NULL, $3, $4, $5, false) RETURNING *;";
 export const createMobileSubscriptionQuery =
-  "insert into Subscriptions(Provider, Type, Category, Name, PriceMonth, DataMonth, Talktime, UploadSpeed, DownloadSpeed, Userid, available) VALUES ($1, 'Mobile', 'Mobile', 'Placeholder', $2, $3, $4, NULL, NULL, $5, false) RETURNING *;";
+  "insert into Subscriptions(Provider, Type, Name, PriceMonth, DataMonth, Talktime, UploadSpeed, DownloadSpeed, Userid, available) VALUES ($1, 'Mobile', 'Placeholder', $2, $3, $4, NULL, NULL, $5, false) RETURNING *;";
+// Delte queries
 export const deleteMobileSubscriptionQuery =
   "DELETE FROM Subscriptions WHERE subscriptionId = $1;";
-export const addUserQuery =
-  "INSERT INTO users(userID) VALUES ($1) RETURNING *;";
-
-export const getSubscription =
-  "SELECT * FROM Subscriptions WHERE subscriptionId = $1;";
 
 {
   /*
---
+++ src/pages/api/createBroadbandSubscription.js
@@ -0,0 +1,28 @@
+import { pool } from "../../../db";
+import { createBroadbandSubscriptionQuery } from "../../../apiQueries";
+
+const createBroadbandSubscription = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+
+  const { Provider, PriceMonth, uploadSpeed, downloadSpeed, Userid } = req.body;
+
+  try {
+    const results = await pool.query(createBroadbandSubscriptionQuery, [
+      Provider,
+      PriceMonth,
+      uploadSpeed,
+      downloadSpeed,
+      Userid,
+    ]);
+    // Assuming the inserted row is the first row in the result set
+    const insertedRow = results.rows[0];
+    res.status(201).json(insertedRow); // Send the inserted row back to the client
+  } catch (error) {
+    console.error("Error creating broadband subscription:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export default createBroadbandSubscription;
