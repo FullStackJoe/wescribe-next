f3bbcd1d305de4088dc3a5d708fca11ad63f911d
Johan Nielsen <johanhn10@gmail.com>
2023-11-14 22:55:06 +0100

    analyitcs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,12 @@
 import { AuthProvider } from "@/firebase/AuthContext";
-import Navbar from "@/components/Navbar/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
+      <Analytics />
     </AuthProvider>
   );
 }
